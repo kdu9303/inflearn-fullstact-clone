d951cdaec788f2d872f25bcfe72ab437e72fe4b1
unknown <zheros9303@gmail.com>
2026-07-19 20:51:03 +0900

    feat: section, lecture api 구현

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,8 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { CoursesModule } from './courses/courses.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { LecturesModule } from './lectures/lectures.module';
+import { SectionsModule } from './sections/sections.module';
 
 @Module({
   imports: [
@@ -12,6 +14,8 @@ import { PrismaModule } from './prisma/prisma.module';
     AuthModule,
     CoursesModule,
     PrismaModule,
+    LecturesModule,
+    SectionsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
